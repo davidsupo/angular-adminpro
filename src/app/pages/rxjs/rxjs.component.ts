b9e84902198d9357325c4e2212a908597386282d
David Supo <davidjsupoo@gmail.com>
2020-08-04 13:29:59 -0500

    Breadcrumbs terminados

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: [
+  ]
+})
+export class RxjsComponent {
+  constructor(){
+
+  }
+}
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, ActivationEnd, Data } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy{
 
-  constructor() { }
+  public titulo: string;
+  public tituloSubs$: Subscription;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.tituloSubs$ = this.getArgumentsRoute()
+                      .subscribe(({ title }) => {
+                        this.titulo = title;
+                        document.title = `AdminPro | ${title}`;
+                      });
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentsRoute(): Observable<Data>{
+    return this.router.events
+    .pipe(
+      filter(event => event instanceof ActivationEnd),
+      filter( (event: ActivationEnd) => event.snapshot.firstChild === null),
+      map( (event: ActivationEnd) => event.snapshot.data)
+    );
   }
 
 }
